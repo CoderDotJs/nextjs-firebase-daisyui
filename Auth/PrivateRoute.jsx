18c85c32c98bd37f543b6298b20512f25250ec9d
CoderDotJs <md.akash6724@gmail.com>
2022-08-26 20:03:06 +0600

    build: added loading

--- Auth/PrivateRoute.jsx
+++ Auth/PrivateRoute.jsx
@@ -1,11 +1,10 @@
 import { useRouter } from 'next/router';
-import 'nprogress/nprogress.css'; //styles of nprogress
 import { useEffect } from 'react';
 import Loading from '../components/Loading/Loading';
 import useAuth from './useAuth';
 
 const ProtectedRoute = ({ children }) => {
-  const { user, loading, dbUserLoading, dbUser } = useAuth();
+  const { user, loading } = useAuth();
   const router = useRouter();
   //no user then login
   useEffect(() => {
@@ -13,23 +12,14 @@ const ProtectedRoute = ({ children }) => {
       router.push('/login');
     }
   }, [router, user, loading]);
-  //if user is loading show bar loading
-  // useEffect(() => {
-  //   if (loading || dbUserLoading) {
-  //     NProgress.start();
-  //   }
-  //   return () => {
-  //     NProgress.done();
-  //   };
-  // }, [loading, dbUserLoading]);
 
   //if useris loading show full screen loading
 
-  if (loading || dbUserLoading) {
+  if (loading) {
     return <Loading />;
   }
 
-  return <>{dbUser?.email ? children : null}</>;
+  return <>{user ? children : null}</>;
 };
 
 export default ProtectedRoute;
